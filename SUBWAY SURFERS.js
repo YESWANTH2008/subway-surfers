8e99e0681bffe344feaa81c89840abcda9d4f826
YESWANTH2008 <87262046+YESWANTH2008@users.noreply.github.com>
2021-08-23 20:23:42 +0530

    Add files via upload

---
+++ SUBWAY SURFERS.js
@@ -0,0 +1,53 @@
+var runner, runner_running, bomb,coin,energyDrink, path, power, 
+ 
+  runner=loadAnimation("Runner-1.png","Runner-2.png");
+  path=loadImage("path.png");
+  coin=loadImage("coin.png");
+
+
+function setup(){
+  createCanvas(400,400);
+  path=createSprite(200,200);
+  path.addImage("path.png");
+  path.velocityY=5;
+  path.y=path.height/30;
+
+  runner=createSprite(180,340,50,170);
+  runner.addAnimation("running", "Runner-1.png");
+  
+  coin=createSprite(200,200,80,90);
+  coin.addImage("coin.png");
+  coin.scale=0.3
+
+  coin=createSprite(100,200,80,90);
+  coin.addImage("coin.png");
+  coin.scale=0.3
+  
+  left_boundary=createSprite(0,300,100,600);
+  right_boundary=createSprite(390,300,80,600);
+ left_boundary.visible=false;
+right_boundary.visible=false;
+}
+
+function draw() {
+  background(0);
+  textSize(10);
+  text('coin Score', 1,200);
+  textSize(10);
+  text('0', 20,230);
+  path.velocityY=5;
+  runner.x=World.mouseX;
+  if(path.y>400) {
+  path.y=height/2 ;
+  if(runner.isTouching(coin)) {
+  coinscore = coinscore+1;
+ }
+  
+}
+  runner.collide(left_boundary);
+  runner.collide(right_boundary);
+  edges=createEdgeSprites();
+  runner.collide(edges[3]);
+drawSprites();
+}
+
